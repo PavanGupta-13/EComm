b89017281c04f8ebcafc090e8549e065db5bb47d
PavanGupta-13 <pavanguptak13@gmai.com>
2021-12-28 12:05:47 +0530

    Added Heading

--- src/login/LoginPage.js
+++ src/login/LoginPage.js
@@ -4,6 +4,7 @@ import './LoginPage.css'
 function LoginPage() {
     return (
         <div className="Parent">
+            <div id="Heading"><h1>User Login</h1></div>
             <div className="Child">
             <h4 className="Labels">UserName</h4>
             <input type="text-box"></input>
